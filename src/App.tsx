694cdce98beb802dfbd808f1c837a846e7c76990
avdhoot <avdhoothadke@gmail.com>
2025-10-30 23:12:35 +0530

    home page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   )
 }
